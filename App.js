4bcebe38850cdc48e33ba8af15112be9561e8ef1
MALIKKHUBAIB1 <khubaib9221@gmail.com>
2025-10-28 00:10:22 +0530

    installing and setup react native

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Hello World!</Text>
+      <Text>Another pice of text</Text>
     </View>
   );
 }
@@ -13,8 +13,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#f0e8e8ff",
+    alignItems: "center",
+    justifyContent: "center",
+    color: "blue",
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "red", // ✅ define color separately
+    padding: 4, // ✅ number, not string
+    margin: 2, // ✅ number, not string
+    width: 200, // optional: adds better layout
+    borderRadius: 4,
   },
 });
